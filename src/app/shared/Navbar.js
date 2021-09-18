bf69e3c54a9c3e959c2fc7e2591a64846e10583a
Aleksandar Stojkov <a11946910@unet.univie.ac.at>
2021-09-18 12:37:28 +0200

    Navbar mobile added first commit

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -8,7 +8,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
-
+import MediaQuery from 'react-responsive';
 export function Navbar(props) {
     const theme = createMuiTheme({
         typography: {
@@ -26,6 +26,15 @@ export function Navbar(props) {
             ].join(','),
         },
     });
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <div className="container">
@@ -59,39 +68,89 @@ export function Navbar(props) {
                         </IconButton>
                         <a href="tel:+38970332752" style={{ fontSize: '120%', color: '#6c6d6a' }}>+38970332752</a>
                     </div>
-                    <div style={{ position: "absolute", right: '1%' }} className="pull-right">
-                        <div >
-                            <ThemeProvider theme={theme}>
-                                <List className="listFont" style={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
-                                    <ListItem>
-                                        <Link to="/">
-                                            <Button>
-                                                <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Home</b></Typography>
-                                            </Button>
-                                        </Link>
+                    <MediaQuery minDeviceWidth={1224}>
+                        <div style={{ position: "absolute", right: '1%' }} className="pull-right">
+                            <div >
+                                <ThemeProvider theme={theme}>
+                                    <List className="listFont" style={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
+                                        <ListItem>
+                                            <Link to="/">
+                                                <Button>
+                                                    <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Home</b></Typography>
+                                                </Button>
+                                            </Link>
 
-                                    </ListItem>
-                                    <ListItem>
-                                        <Link to="/about_us">
-                                            <Button>
-                                                <Typography noWrap variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }}><b>About us</b></Typography></Button></Link>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Link to="/contact">
-                                            <Button>
-                                                <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Contact</b></Typography>
-                                            </Button></Link>
-                                    </ListItem>
-                                    <ListItem>
-                                        <Link to="/faq">
-                                            <Button>
-                                                <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>FAQ</b></Typography>
-                                            </Button></Link>
-                                    </ListItem>
-                                </List>
-                            </ThemeProvider>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Link to="/about_us">
+                                                <Button>
+                                                    <Typography noWrap variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }}><b>About us</b></Typography></Button></Link>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Link to="/contact">
+                                                <Button>
+                                                    <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Contact</b></Typography>
+                                                </Button></Link>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Link to="/faq">
+                                                <Button>
+                                                    <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>FAQ</b></Typography>
+                                                </Button></Link>
+                                        </ListItem>
+                                    </List>
+                                </ThemeProvider>
+                            </div>
                         </div>
-                    </div>
+                    </MediaQuery><MediaQuery maxDeviceWidth={1224}>
+                        <div>
+                            <IconButton
+                                aria-label="more"
+                                id="long-button"
+                                aria-controls="long-menu"
+                                aria-expanded={open ? 'true' : undefined}
+                                aria-haspopup="true"
+                                onClick={handleClick}
+                            >
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu
+                                id="long-menu"
+                                MenuListProps={{
+                                    'aria-labelledby': 'long-button',
+                                }}
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem>
+                                    <Link to="/">
+                                        <Button>
+                                            <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Home</b></Typography>
+                                        </Button>
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Link to="/about_us">
+                                        <Button>
+                                            <Typography noWrap variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }}><b>About us</b></Typography></Button></Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Link to="/contact">
+                                        <Button>
+                                            <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>Contact</b></Typography>
+                                        </Button></Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Link to="/faq">
+                                        <Button>
+                                            <Typography variant="h6" style={{ textAlign: 'center', color: "#4a4a49" }} ><b>FAQ</b></Typography>
+                                        </Button></Link>
+                                </MenuItem>
+                            </Menu>
+                        </div>
+                    </MediaQuery>
+
                 </Toolbar>
 
             </AppBar>
